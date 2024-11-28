83755c0d01055f84c3263d4c851040172f6b992e
Lunity111 <lunasolisort@gmail.com>
2024-11-28 08:34:03 -0600

    Add files via upload

--- js/funciones.js
+++ js/funciones.js
@@ -1,23 +1,37 @@
 const app = Vue.createApp({
+    mounted() {
+        // Llamada a la API al montar la instancia
+   },
+
+   created(){
+       this.cargarMuebles();
+   },
+
     data() {
         return {
             muebles: [], 
             carrito: [], // Carrito para almacenar muebles añadidos
+            
         };
     },
-    mounted() {
-        this.cargarMuebles(); // Llamada a la API al montar la instancia
-    },
+   
     methods: {
         cargarMuebles() {
             axios
                 .get("https://www.course-api.com/react-store-products")
                 .then(response => {
-                    this.muebles = response.data.slice(0, 6); // Obtener solo 6 muebles
+                    this.muebles = response.data.slice(0, 8); // Obtener solo 6 muebles
+                    let i=5;
+                for(mueble of this.muebles){
+                    mueble.cantidad = i;
+                    this.muebles.mueble;
+                    i=Math.floor(Math.random()*(50-5)+5);
+                }
                 })
                 .catch(error => {
                     console.error("Error al cargar los muebles:", error);
                 });
+                
         },
         verMas(mueble) {
             alert(`Detalle de ${mueble.name}:\n\nDescripción: ${mueble.description}\nPrecio: $${mueble.price}`);
